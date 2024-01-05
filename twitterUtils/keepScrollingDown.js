10f05cf7b3891fbca502b7a4ae88b276a8a9006f
nabilHaouam <workmailnabil@gmail.com>
2024-01-05 14:12:24 +0100

    closing the page if no content is loaded on scroll

--- twitterUtils/keepScrollingDown.js
+++ twitterUtils/keepScrollingDown.js
@@ -2,16 +2,35 @@ const puppeteer = require('puppeteer');
 const config = require('../config.json');
 
 let scrollInterval;
+let lastHeight = 0;
+let consecutiveSameHeightCount = 0;
+const maxConsecutiveSameHeightCount = 3; 
+
 async function keepScrollingDown(page) {
-    scrollInterval = setInterval(async () => {
-      await page.evaluate(() => {
-        window.scrollTo(0, document.body.scrollHeight);
-      });
-    }, config.scrollInterval);
-  
-    return scrollInterval;
-  }
-  
-  module.exports = {
-    keepScrollingDown: keepScrollingDown
-  };
+  scrollInterval = setInterval(async () => {
+    await page.evaluate(() => {
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+
+    const newHeight = await page.evaluate(() => document.body.scrollHeight);
+    if (newHeight === lastHeight) {
+      consecutiveSameHeightCount++;
+    } else {
+      consecutiveSameHeightCount = 0;
+    }
+
+    if (consecutiveSameHeightCount >= maxConsecutiveSameHeightCount) {
+      // No more new content is being loaded, close the page
+      clearInterval(scrollInterval);
+      await page.close();
+    }
+
+    lastHeight = newHeight;
+  }, config.scrollInterval); 
+
+  return scrollInterval;
+}
+
+module.exports = {
+  keepScrollingDown: keepScrollingDown
+};
